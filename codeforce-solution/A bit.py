10ac97418da19f80d6b032839a50d7114ed9f20f
Edldev12 <edlawitdigital3@gmail.com>
2026-05-22 14:19:34 +0300

    A bit solution in codeforce

---
+++ codeforce-solution/A bit.py
@@ -0,0 +1,14 @@
+X=0
+y=int(input())
+for i in range(y):
+    z=input()
+    if z=="++X":
+        X+=1
+    elif z=="X++":
+        X+=1
+    elif z=="X--":
+        X-=1
+    else:
+        X-=1
+   
+print(X)
