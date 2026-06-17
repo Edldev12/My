6918222307b76ee6f895a60c89f510c671669b4d
Edldev12 <edlawitdigital3@gmail.com>
2026-06-17 13:17:47 +0300

    add codeforce-solution

--- A_Calculating_Function.py
+++ A_Calculating_Function.py
@@ -1,11 +1,5 @@
-x=int(input())
-counter=0
-if x==0:
-    counter=0
+n = int(input())
+if n % 2 == 0:
+    print(n // 2)
 else:
-    for i in range(1,x+1):
-        if i%2==0:
-            counter+=i
-        else:
-            counter-=i
-print(counter)
+    print(-(n + 1) // 2)
