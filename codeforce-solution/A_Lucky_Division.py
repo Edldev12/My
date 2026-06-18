e70b74e769de49897e32c236b1ea6eac3d3d2d08
Edldev12 <edlawitdigital3@gmail.com>
2026-06-18 10:24:58 +0300

    Add Drinks solution to codeforce-solution folder

--- codeforce-solution/A_Lucky_Division.py
+++ codeforce-solution/A_Lucky_Division.py
@@ -1,20 +1,11 @@
-
 import sys
-
 def solve():
-    # Read the integer input
     n = int(sys.stdin.read().strip())
-    
-    # List of all lucky numbers up to 1000
     lucky_numbers = [4, 7, 44, 47, 74, 77, 444, 447, 474, 477, 744, 747, 774, 777]
-    
-    # Check if n is divisible by any lucky number
     for lucky in lucky_numbers:
         if n % lucky == 0:
             print("YES")
-            return
-            
+            return        
     print("NO")
-
 if __name__ == '__main__':
     solve()
