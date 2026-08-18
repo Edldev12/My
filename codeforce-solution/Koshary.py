3e0098def6e407aa3d5ca0d6e4dee8e8b8371231
Edlawit Tsegaye <edlawitdigital3@gmail.com>
2026-08-18 14:14:05 +0300

    Add Koshary.py solution for coordinate problem

---
+++ codeforce-solution/Koshary.py
@@ -0,0 +1,27 @@
+import sys
+
+def solve():
+    input = sys.stdin.read
+    data = input().split()
+    if not data:
+        return
+    
+    t = int(data[0])
+    results = []
+    
+    idx = 1
+    for _ in range(t):
+        x = int(data[idx])
+        y = int(data[idx+1])
+        idx += 2
+        
+        # If both coordinates are odd, it is impossible.
+        if x % 2 == 1 and y % 2 == 1:
+            results.append("NO")
+        else:
+            results.append("YES")
+            
+    print('\n'.join(results))
+
+if __name__ == '__main__':
+    solve()
